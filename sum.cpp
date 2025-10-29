bb0eed7b82481b904b7cd209675883c3f5af2117
varshini646 <varshi0088@gmail.com>
2025-10-29 18:45:35 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+//Sample programe
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("sum of two numbers=%d",sum);
+	return 0;
+}

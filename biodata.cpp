f492caafda1be9e5dd39c41ec3a968ee7d5ca439
varshini646 <varshi0088@gmail.com>
2025-10-29 18:46:49 +0530

    Add files via upload

---
+++ biodata.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("NAME:P.VARSHINI");
+	printf("\nDOB:25-01-20008");
+	printf("\nPLACE:PALNADU");
+	printf("\nFATHER NAME:P.RAJA SEKHAR REDDY\n");
+	return 0;
+}

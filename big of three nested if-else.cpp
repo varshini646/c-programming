4d8ec0046a0a2ae0eabde13c2d2a33dae37b6bf2
varshini646 <varshi0088@gmail.com>
2025-10-30 09:30:50 +0530

    Add files via upload

---
+++ big of three nested if-else.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,z;
+	printf("enter x and y and z values");
+	scanf("%d%d%d",&x,&y,&z);
+	if(x>y)
+	if(x>z)
+	printf("x is big");
+	else
+	printf("z is big");
+	else
+	if(y>z)
+	printf("y is big");
+	else
+	printf("z is big");
+	return 0;
+}

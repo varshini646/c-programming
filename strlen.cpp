317306cdbbced77a3c201485b7bb17239c997803
varshini646 <varshi0088@gmail.com>
2025-11-25 21:23:53 +0530

    Add files via upload

---
+++ strlen.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+	char i,s[20];
+	int l;
+	printf("enter string name");
+	gets(s);
+	l=strlen(s);
+	printf("length of the string=%d",l);
+	return 0;
+}

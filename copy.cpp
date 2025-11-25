ecfca333ed98f4d5d675f164cdcc3d04505b8e72
varshini646 <varshi0088@gmail.com>
2025-11-25 21:25:00 +0530

    Add files via upload

---
+++ copy.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char i,s1[100],s2[100];
+	printf("enter string 1 name");
+	scanf("%s",s);
+	strcpy(s2,s1);
+	printf("original string %s",s1);
+	printf("copied string is %s",s2);
+	return 0;
+}
